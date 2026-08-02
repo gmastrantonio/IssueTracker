cc1de421bd108a0c1fff8f52e8f6cf4afea95813
Giuseppe Mastrantonio <giuseppe.mastrantonio1@gmail.com>
2026-08-02 11:11:29 +0200

    more gitignore

--- IssueTracker/IssueTracker.Client/obj/Debug/net8.0/IssueTracker.Client.AssemblyInfo.cs
+++ IssueTracker/IssueTracker.Client/obj/Debug/net8.0/IssueTracker.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IssueTracker.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27b276e15452a63cdd956513aeb856650cbe7ce7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da906122cca5598bc8c3860d1c57afe6f9a06c1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("IssueTracker.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IssueTracker.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- IssueTracker/IssueTracker.Core/obj/Debug/net8.0/IssueTracker.Core.AssemblyInfo.cs
+++ IssueTracker/IssueTracker.Core/obj/Debug/net8.0/IssueTracker.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IssueTracker.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b180722f61017463d4ecf470371ef0c3cd20f7cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da906122cca5598bc8c3860d1c57afe6f9a06c1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("IssueTracker.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IssueTracker.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
